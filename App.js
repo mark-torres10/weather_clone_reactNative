a472e4848d5ff713517dbc186fcf8d74924f5c69
mark-torres-10 <markptorres1@gmail.com>
2020-12-07 19:15:02 -0500

    start getting location

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [errorMessage, setErrorMessage] = useState(null); 
+  // run at start
+  useEffect(()=> {
+    // use async load function
+    load()
+  }, []); 
+
+  async function load() {
+    try {
+
+      // get permission to get location
+      let { status } = await Location.requestPermissionsAsync(); 
+
+      if(status != "granted") {
+        setErrorMessage("Access to the location is needed in order to run the app"); 
+        return
+      }
+
+      // get location
+      const location = await Location.getCurrentPositionAsync();
+      
+      const {latitude, longitude} = location.coords; 
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`); 
+
+    } catch (error) {
+
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to your new app!</Text>
       <StatusBar style="auto" />
     </View>
   );
