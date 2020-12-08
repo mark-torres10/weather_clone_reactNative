ea0b9111f40445e01f5c2e01692f0f17b9b8f236
mark-torres-10 <markptorres1@gmail.com>
2020-12-07 19:56:21 -0500

    access openweather API

--- App.js
+++ App.js
@@ -3,9 +3,15 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
 
+/*import { WEATHER_API_KEY } from 'react-native-dotenv'; */
+
+const WEATHER_API_KEY = "ce00af567f3ebd4645b36309641d801a"
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+
 export default function App() {
 
   const [errorMessage, setErrorMessage] = useState(null); 
+  const [currentWeather, setCurrentWeather] = useState(null); 
   // run at start
   useEffect(()=> {
     // use async load function
@@ -29,18 +35,43 @@ export default function App() {
       const {latitude, longitude} = location.coords; 
       alert(`Latitude: ${latitude}, Longitude: ${longitude}`); 
 
-    } catch (error) {
+      // make call to OpenWeather API
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`; 
 
+      const response = await fetch(weatherUrl); 
+
+      const result = await response.json(); 
+
+      if(response.ok) {
+        setCurrentWeather(result); 
+      } else {
+        setErrorMessage(result.message);
+      }
+    } catch (error) {
+      alert(`Unable to load page due to the following error: ${error}`); 
     }
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Welcome to your new app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  if(currentWeather) {
+
+    const { main : {temp}} = currentWeather; 
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Welcome to your new app!</Text>
+        {/*<Text>Today's temperature: {temp}</Text>*/}
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto"/>
+      </View>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Simple weather app, built using React Native
 
 Inspired by the following (amazing) tutorial: https://www.youtube.com/watch?v=NgDaPmxewcg
+
+Uses OpenWeather API to get current weather information: https://home.openweathermap.org/
